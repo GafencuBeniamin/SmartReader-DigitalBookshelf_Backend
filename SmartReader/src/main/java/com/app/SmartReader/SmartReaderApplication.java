fe30ca044e9bf9008af5d8812709d5b7a908d94f
Gafencu Beniamin <99902312+GafencuBeniamin@users.noreply.github.com>
2024-04-02 14:59:25 +0300

    Initial commit and setup

---
+++ SmartReader/src/main/java/com/app/SmartReader/SmartReaderApplication.java
@@ -0,0 +1,13 @@
+package com.app.SmartReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartReaderApplication.class, args);
+	}
+
+}
---
+++ SmartReader/src/test/java/com/app/SmartReader/SmartReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SmartReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
